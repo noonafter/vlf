4c7badaec3052bcd34599db64ee7b170e38d0b39
lc <lichuan0987@qq.com>
2024-11-29 16:47:15 +0800

    1.加上主窗口中udp接收线程工作类的代码

---
+++ src/app/recv_worker.cpp
@@ -0,0 +1,59 @@
+//
+// Created by noisemonitor on 2024/11/28.
+//
+
+#include <QNetworkDatagram>
+#include "recv_worker.h"
+
+
+recv_worker::recv_worker(QObject *parent) : QObject(parent){
+
+    udp_recv = new QUdpSocket(this);
+    if(udp_recv->bind(QHostAddress::LocalHost, 55555)){
+        qDebug() << "Bind success";
+    }else {
+        qDebug() << "Udp socket bind fail: " << udp_recv->errorString();
+    };
+    // 1MB udp buffer
+    udp_recv->setSocketOption(QAbstractSocket::ReceiveBufferSizeSocketOption,  1<<20);
+    connect(udp_recv, &QUdpSocket::readyRead, this, &recv_worker::slot_udp_recv_readyRead);
+
+    file = new QFile(R"(E:\project\vlf\src\udp_modem\sig_sum_int)");
+    if(!file->open(QIODevice::WriteOnly | QIODevice::Append)){
+        qWarning() << "failed to open file" << file->errorString();
+        return;
+    }
+    out = new QDataStream(file);
+
+}
+
+recv_worker::~recv_worker() {
+
+    if(file)
+        file->close();
+    delete file;
+    delete out;
+}
+
+void recv_worker::slot_udp_recv_readyRead() {
+//    qDebug() << "slot udp recv";
+
+
+    while (udp_recv->hasPendingDatagrams()){
+        QNetworkDatagram datagram = udp_recv->receiveDatagram();
+
+        // 处理datagram
+        out->writeRawData(datagram.data(),datagram.data().size());
+        count_rev++;
+    }
+
+    if(!(count_rev % 100)){
+        qDebug() << "count_rev is" << count_rev;
+//        qDebug() << "recv buff is " << udp_recv->socketOption(QAbstractSocket::ReceiveBufferSizeSocketOption);
+    }
+
+
+
+
+
+}
